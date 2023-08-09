72af59fa39594682cc9ab6d7fa9d23b4b8e119e4
sadamm27 <sadammochammad409@gmail.com>
2023-08-09 17:10:53 +0700

    commit blog app mini project

---
+++ api/index.js
@@ -0,0 +1,173 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const User = require("./models/User");
+const bcrypt = require("bcryptjs");
+const Post = require("./models/Post");
+const mongoose = require("mongoose");
+const salt = bcrypt.genSaltSync(10);
+const jwt = require("jsonwebtoken");
+const secret = "asnfoanvovwojfoqef";
+const cookieParser = require("cookie-parser");
+const multer = require("multer");
+const uploadMiddleware = multer({ dest: "uploads/" });
+const fs = require("fs");
+
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(express.json());
+app.use(cookieParser());
+app.use("/uploads", express.static(__dirname + "/uploads"));
+mongoose.connect(
+  "mongodb+srv://BlogIndonesia:BlogIndonesia@cluster0.2pwasae.mongodb.net/?retryWrites=true&w=majority"
+);
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    //logged in
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (error, token) => {
+      if (error) throw error;
+      res.cookie("token", token).json({
+        id: userDoc._id,
+        username,
+      });
+    });
+  } else {
+    res.status(400).json("Kredensial Salah");
+  }
+});
+
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (error, info) => {
+    if (error) throw error;
+    res.json(info);
+  });
+  res.json(req.cookies);
+});
+
+app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
+  const { originalname, path } = req.file;
+  const parts = originalname.split(".");
+  const ext = parts[parts.length - 1];
+  const newPath = path + "." + ext;
+  fs.renameSync(path, newPath);
+
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { title, summary, content } = req.body;
+    const postDoc = await Post.create({
+      title,
+      summary,
+      content,
+      cover: newPath,
+      author: info.id,
+    });
+    res.json(postDoc);
+  });
+});
+
+app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
+  let newPath = null;
+  if (req.file) {
+    const { originalname, path } = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { id, title, summary, content } = req.body;
+
+    try {
+      const postDoc = await Post.findById(id);
+
+      if (!postDoc) {
+        return res.status(404).json('Post not found');
+      }
+
+      const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+      if (!isAuthor) {
+        return res.status(400).json('you are not the author');
+      }
+
+      await Post.updateOne(
+        { _id: id },
+        {
+          $set: {
+            title,
+            summary,
+            content,
+            cover: newPath ? newPath : postDoc.cover,
+          },
+        }
+      );
+
+      // Fetch the updated postDoc after the update
+      const updatedPostDoc = await Post.findById(id);
+
+      res.json(updatedPostDoc); // Send the updated postDoc in the response
+    } catch (error) {
+      console.error(error);
+      res.status(500).json('Internal server error');
+    }
+  });
+});
+
+app.get("/post", async (req, res) => {
+  res.json(
+    await Post.find()
+      .populate("author", ["username"])
+      .sort({ createdAt: -1 })
+      .limit(20)
+  );
+});
+
+app.get("/post/:id", async (req, res) => {
+  const { id } = req.params;
+  const postDoc = await Post.findById(id).populate("author", ["username"]);
+  res.json(postDoc);
+});
+
+app.delete("/post/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const postDoc = await Post.findById(id);
+    
+    if (!postDoc) {
+      return res.status(404).json('Post not found');
+    }
+    
+    // Implement additional authorization logic if needed, e.g., checking if the user is the author of the post
+
+    await postDoc.remove(); // Delete the post
+
+    res.json('Post deleted successfully');
+  } catch (error) {
+    console.error(error);
+    res.status(500).json('Internal server error');
+  }
+});
+
+
+app.listen(4000);
